98b19cfddd43eb7401f2ea3973b45c948fa2a068
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-04 17:29:32 +0530

    broke up index to multiple files

--- site/pages/_meta.js
+++ site/pages/_meta.js
@@ -1,4 +1,11 @@
 export default {
   index: 'Introduction',
+  'get-started': 'Get Started',
+  'core-tenets': 'Core Tenets',
+  'config-as-data': 'Configuration as Data',
+  'key-features': 'Key Features',
+  'use-cases': 'Use Cases',
+  'compare': 'Compare',
+  'faq': 'FAQ',
   "platform": 'Platforms'
 }
