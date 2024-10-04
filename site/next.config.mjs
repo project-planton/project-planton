54d7ac6da29c0b50e4eb99e0308e352f0002ca58
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-04 17:04:22 +0530

    added more pages under kubernetes

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -5,7 +5,7 @@ const withNextra = nextra({
     themeConfig: './theme.config.jsx'
 })
 
-export default withNextra()
+export default withNextra({})
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // export default withNextra({ /* other next.js config */ })
---
+++ site/pages/platforms/kubernetes/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  'kafka-kubernetes': 'Kafka',
+  'microservice-kubernetes': 'Microservice',
+  'postgres-kubernetes': 'Postgres',
+  'redis-kubernetes': 'Redis',
+}
