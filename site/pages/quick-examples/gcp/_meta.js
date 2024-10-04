66df35f0e7f809578b0450158a6f6a4d2e2a908a
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-04 16:17:25 +0530

    added more docs

---
+++ site/pages/quick-examples/gcp/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  "gcp-artifact-registry": 'Artifact Registry',
+  "gcp-dns-zone": 'CloudDNS Zone',
+  "gcp-secrets-manager": 'Secrets Manager',
+  "gcs-bucket": 'Cloud Storage Bucket',
+  "gke-cluster": 'GKE Cluster',
+}
