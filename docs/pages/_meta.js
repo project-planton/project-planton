c74c69a2dd206996afd1fb22524b9feeff4a586b
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-05 13:33:00 +0530

    added pulumi-modules to the site and renamed site folder to docs

--- docs/pages/_meta.js
+++ docs/pages/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: 'Introduction',
   'get-started': 'Get Started',
   'core-tenets': 'Core Tenets',
+  'pulumi-modules': 'Pulumi Modules',
   'config-as-data': 'Configuration as Data',
   'key-features': 'Key Features',
   'use-cases': 'Use Cases',
