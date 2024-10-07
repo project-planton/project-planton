b5a0f492f22aedab12c703a5961f1ded2f458eb0
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-07 19:39:06 +0530

    added kind-friendly redis manifest

--- internal/manifest/load_manifest.go
+++ internal/manifest/load_manifest.go
@@ -2,17 +2,36 @@ package manifest
 
 import (
 	"github.com/pkg/errors"
+	"github.com/plantoncloud/project-planton/internal/ulidgen"
+	"github.com/plantoncloud/project-planton/internal/workspace"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"path/filepath"
 	"sigs.k8s.io/yaml"
 )
 
 func LoadManifest(manifestPath string) (proto.Message, error) {
+	isUrl, err := isManifestPathUrl(manifestPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to determine if manifest path is url")
+	}
+
+	if isUrl {
+		manifestPath, err = downloadManifest(manifestPath)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to download manifest using %s", manifestPath)
+		}
+	}
+
 	manifestYamlBytes, err := os.ReadFile(manifestPath)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to read manifest file")
+		return nil, errors.Wrapf(err, "failed to read manifest file %s", manifestPath)
 	}
+
 	jsonBytes, err := yaml.YAMLToJSON(manifestYamlBytes)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to load yaml to json")
@@ -30,7 +49,58 @@ func LoadManifest(manifestPath string) (proto.Message, error) {
 	}
 
 	if err := protojson.Unmarshal(jsonBytes, manifest); err != nil {
-		return nil, errors.Wrap(err, "failed to load json into proto message")
+		return nil, errors.Wrapf(err, "failed to load json into proto message from %s", manifestPath)
 	}
 	return manifest, nil
+}
+
+func downloadManifest(manifestUrl string) (string, error) {
+	// Get the directory to save the downloaded file
+	dir, err := workspace.GetManifestDownloadDir()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get manifest download directory")
+	}
+
+	// Generate a new ulid for the file name
+	fileName := ulidgen.NewGenerator().Generate().String() + ".yaml"
+
+	filePath := filepath.Join(dir, fileName)
+
+	// Create the file
+	out, err := os.Create(filePath)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create file")
+	}
+	defer out.Close()
+
+	// Download the file
+	resp, err := http.Get(manifestUrl)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to download manifest from %s", manifestUrl)
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to write manifest to file")
+	}
+
+	// Return the absolute path of the downloaded file
+	return filePath, nil
+}
+
+func isManifestPathUrl(manifestPath string) (bool, error) {
+	// Attempt to parse the manifestPath as a URL
+	parsedUrl, err := url.Parse(manifestPath)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to parse manifest path as URL")
+	}
+
+	// Check if the URL has a scheme and host
+	if parsedUrl.Scheme == "" || parsedUrl.Host == "" {
+		return false, nil
+	}
+
+	return true, nil
 }
--- internal/workspace/workspace.go
+++ internal/workspace/workspace.go
@@ -47,5 +47,11 @@ func GetManifestDownloadDir() (string, error) {
 	if err != nil {
 		return "", errors.Wrap(err, "failed to get home directory")
 	}
+	dir := filepath.Join(homeDir, ProjectPlantonDir, "downloads")
+	if !isDirExists(dir) {
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return "", errors.Wrapf(err, "failed to ensure %s dir", dir)
+		}
+	}
 	return filepath.Join(homeDir, ProjectPlantonDir, "downloads"), nil
 }
