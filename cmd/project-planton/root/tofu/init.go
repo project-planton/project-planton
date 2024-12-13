64f3e65ec520e7554c0fc3f31fbc87425d6f6b09
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-12-13 17:04:37 +0530

    fixed tofu init as well

--- cmd/project-planton/root/tofu/init.go
+++ cmd/project-planton/root/tofu/init.go
@@ -3,6 +3,7 @@ package tofu
 import (
 	"github.com/project-planton/project-planton/apis/project/planton/shared/tofu"
 	"github.com/project-planton/project-planton/internal/cli/flag"
+	"github.com/project-planton/project-planton/internal/iac/pulumi/stackinput/credentials"
 	"github.com/project-planton/project-planton/internal/iac/tofu/tofumodule"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -15,10 +16,35 @@ var Init = &cobra.Command{
 }
 
 func initHandler(cmd *cobra.Command, args []string) {
+	inputDir, err := cmd.Flags().GetString(string(flag.InputDir))
+	flag.HandleFlagErr(err, flag.InputDir)
+
 	moduleDir, err := cmd.Flags().GetString(string(flag.ModuleDir))
 	flag.HandleFlagErrAndValue(err, flag.ModuleDir, moduleDir)
 
-	err = tofumodule.Run(moduleDir, "", tofu.TofuOperationType_init, map[string]string{})
+	valueOverrides, err := cmd.Flags().GetStringToString(string(flag.Set))
+	flag.HandleFlagErr(err, flag.Set)
+
+	credentialOptions := make([]credentials.StackInputCredentialOption, 0)
+	targetManifestPath := inputDir + "/target.yaml"
+
+	if inputDir == "" {
+		targetManifestPath, err = cmd.Flags().GetString(string(flag.Manifest))
+		flag.HandleFlagErrAndValue(err, flag.Manifest, targetManifestPath)
+
+		credentialOptions, err = credentials.BuildWithFlags(cmd.Flags())
+		if err != nil {
+			log.Fatalf("failed to build credentiaal options: %v", err)
+		}
+	} else {
+		credentialOptions, err = credentials.BuildWithFlags(cmd.Flags())
+		if err != nil {
+			log.Fatalf("failed to build credentiaal options: %v", err)
+		}
+	}
+
+	err = tofumodule.Run(moduleDir, targetManifestPath, tofu.TofuOperationType_init, valueOverrides,
+		credentialOptions...)
 	if err != nil {
 		log.Fatalf("failed to run pulumi: %v", err)
 	}
