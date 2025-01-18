5be991b6d78f815e000229920e2e5c869bc9ad9e
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-01-18 13:26:28 +0530

    set default for aws credentials

--- apis/project/planton/provider/aws/s3bucket/v1/iac/tf/provider.tf
+++ apis/project/planton/provider/aws/s3bucket/v1/iac/tf/provider.tf
@@ -1,16 +1,28 @@
 variable "aws_credential" {
   description = "AWS Credential data, including account_id, access_key_id, secret_access_key, and region. Optional."
   type = object({
-    account_id        = string
     access_key_id     = string
     secret_access_key = string
     region            = string
   })
+  default = {
+    access_key_id     = ""
+    secret_access_key = ""
+    region            = ""
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
-  # If aws_credential is provided, use it; otherwise, fall back to spec.aws_region only.
-  region     = var.aws_credential == null ? var.spec.aws_region : var.aws_credential.region
-  access_key = var.aws_credential == null ? null : var.aws_credential.access_key_id
-  secret_key = var.aws_credential == null ? null : var.aws_credential.secret_access_key
+  region     = var.aws_credential.region != "" ? var.aws_credential.region : var.spec.aws_region
+  access_key = var.aws_credential.access_key_id != "" ? var.aws_credential.access_key_id : null
+  secret_key = var.aws_credential.secret_access_key != "" ? var.aws_credential.secret_access_key : null
 }
