5461de9369970538945daadb6f601663de52fc8e
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-01-19 18:23:34 +0530

    removed aws-credential tf var from s3 bucket for testing

--- apis/project/planton/provider/aws/s3bucket/v1/iac/tf/provider.tf
+++ apis/project/planton/provider/aws/s3bucket/v1/iac/tf/provider.tf
@@ -1,17 +1,3 @@
-variable "aws_credential" {
-  description = "AWS Credential data, including account_id, access_key_id, secret_access_key, and region. Optional."
-  type = object({
-    access_key_id     = string
-    secret_access_key = string
-    region            = string
-  })
-  default = {
-    access_key_id     = null
-    secret_access_key = null
-    region            = null
-  }
-}
-
 terraform {
   required_providers {
     aws = {
@@ -21,8 +7,4 @@ terraform {
   }
 }
 
-provider "aws" {
-  region     = var.aws_credential.region != null ? var.aws_credential.region : var.spec.aws_region
-  access_key = var.aws_credential.access_key_id != null ? var.aws_credential.access_key_id : null
-  secret_key = var.aws_credential.secret_access_key != null ? var.aws_credential.secret_access_key : null
-}
+provider "aws" {}
