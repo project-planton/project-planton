86b30824235bf7e30dcf748260cd78bd51e3aeac
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-01-28 07:17:51 +0530

    updated aws-provider blocks to not deal with credentials

--- apis/project/planton/provider/aws/awslambda/v1/iac/tf/provider.tf
+++ apis/project/planton/provider/aws/awslambda/v1/iac/tf/provider.tf
@@ -1,16 +1,10 @@
-variable "aws_credential" {
-  description = "AWS Credential data, including account_id, access_key_id, secret_access_key, and region. Optional."
-  type = object({
-    account_id        = string
-    access_key_id     = string
-    secret_access_key = string
-    region            = string
-  })
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 5.82.0"
+    }
+  }
 }
 
-provider "aws" {
-  # If aws_credential is provided, use it; otherwise, fall back to spec.aws_region only.
-  region     = var.aws_credential == null ? var.spec.aws_region : var.aws_credential.region
-  access_key = var.aws_credential == null ? null : var.aws_credential.access_key_id
-  secret_key = var.aws_credential == null ? null : var.aws_credential.secret_access_key
-}
+provider "aws" {}
