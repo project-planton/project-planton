fb2e08b63022512997a6fdf18a179fe0f0e08ab6
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-01-31 12:34:43 +0530

    removed local file system backend from pulumi backend cre

--- tests/apis/project/planton/credential/pulumi_backend_credential_test.go
+++ tests/apis/project/planton/credential/pulumi_backend_credential_test.go
@@ -7,19 +7,6 @@ import (
 	"testing"
 )
 
-func TestInitiate_PulumiBackendSpec_Without_LocalFileSystemDetails(t *testing.T) {
-	pulumiBackendCredentialSpec := &pulumibackendcredentialv1.PulumiBackendCredentialSpec{
-		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_local_file_system,
-	}
-	if err := protovalidate.Validate(pulumiBackendCredentialSpec); err != nil {
-		if !strings.Contains(err.Error(), "[local_file_system.mandatory]") {
-			t.Errorf("test failed - validation error of constraint id `local_file_system.mandatory` is expected")
-		}
-	} else {
-		t.Errorf("test failed - validation error expected")
-	}
-}
-
 func TestInitiate_PulumiBackendSpec_Without_HttpDetails(t *testing.T) {
 	pulumiBackendCredentialSpec := &pulumibackendcredentialv1.PulumiBackendCredentialSpec{
 		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_http,
@@ -35,10 +22,10 @@ func TestInitiate_PulumiBackendSpec_Without_HttpDetails(t *testing.T) {
 
 func TestInitiate_PulumiBackendSpec_Without_AwsS3Details(t *testing.T) {
 	pulumiBackendCredentialSpec := &pulumibackendcredentialv1.PulumiBackendCredentialSpec{
-		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_aws_s3,
+		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_s3,
 	}
 	if err := protovalidate.Validate(pulumiBackendCredentialSpec); err != nil {
-		if !strings.Contains(err.Error(), "[aws_s3.mandatory]") {
+		if !strings.Contains(err.Error(), "[s3.mandatory]") {
 			t.Errorf("test failed - validation error of constraint id `aws_s3.mandatory` is expected")
 		}
 	} else {
@@ -48,10 +35,10 @@ func TestInitiate_PulumiBackendSpec_Without_AwsS3Details(t *testing.T) {
 
 func TestInitiate_PulumiBackendSpec_Without_GoogleCloudStorageDetails(t *testing.T) {
 	pulumiBackendCredentialSpec := &pulumibackendcredentialv1.PulumiBackendCredentialSpec{
-		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_google_cloud_storage,
+		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_gcs,
 	}
 	if err := protovalidate.Validate(pulumiBackendCredentialSpec); err != nil {
-		if !strings.Contains(err.Error(), "[google_cloud_storage.mandatory]") {
+		if !strings.Contains(err.Error(), "[gcs.mandatory]") {
 			t.Errorf("test failed - validation error of constraint id `google_cloud_storage.mandatory` is expected")
 		}
 	} else {
@@ -61,10 +48,10 @@ func TestInitiate_PulumiBackendSpec_Without_GoogleCloudStorageDetails(t *testing
 
 func TestInitiate_PulumiBackendSpec_Without_AzureBlobStorageDetails(t *testing.T) {
 	pulumiBackendCredentialSpec := &pulumibackendcredentialv1.PulumiBackendCredentialSpec{
-		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_azure_blob_storage,
+		PulumiBackendType: pulumibackendcredentialv1.PulumiBackendType_azurerm,
 	}
 	if err := protovalidate.Validate(pulumiBackendCredentialSpec); err != nil {
-		if !strings.Contains(err.Error(), "[azure_blob_storage.mandatory]") {
+		if !strings.Contains(err.Error(), "[azurerm.mandatory]") {
 			t.Errorf("test failed - validation error of constraint id `azure_blob_storage.mandatory` is expected")
 		}
 	} else {
