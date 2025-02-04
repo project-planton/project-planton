2a9b7b7c81a5f8c191dddd46b0ee1813c1f0c086
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-02-04 09:41:38 +0530

    implemented terraform module for elasticsearch-kubernetes

---
+++ apis/project/planton/provider/kubernetes/elasticsearchkubernetes/v1/iac/tf/README.md
@@ -0,0 +1,21 @@
+# Terraform Module to Deploy Apache Solr on Kubernetes
+
+```shell
+project-planton tofu init --manifest hack/manifest.yaml --backend-type s3 \
+  --backend-config="bucket=planton-cloud-tf-state-backend" \
+  --backend-config="dynamodb_table=planton-cloud-tf-state-backend-lock" \
+  --backend-config="region=ap-south-2" \
+  --backend-config="key=kubernetes-stacks/test-elasticsearch-cluster.tfstate"
+```
+
+```shell
+project-planton tofu plan --manifest hack/manifest.yaml
+```
+
+```shell
+project-planton tofu apply --manifest hack/manifest.yaml --auto-approve
+```
+
+```shell
+project-planton tofu destroy --manifest hack/manifest.yaml --auto-approve
+```
---
+++ apis/project/planton/provider/kubernetes/elasticsearchkubernetes/v1/iac/tf/ingress.tf
@@ -0,0 +1,350 @@
+# Only create resources if ingress is enabled and dns_domain is non-empty
+resource "kubernetes_manifest" "ingress_certificate" {
+  count = local.ingress_is_enabled && local.ingress_dns_domain != "" ? 1 : 0
+
+  manifest = yamldecode(
+    yamlencode({
+      apiVersion = "cert-manager.io/v1"
+      kind       = "Certificate"
+      metadata = {
+        name      = local.resource_id
+        namespace = local.istio_ingress_namespace
+        labels    = local.final_labels
+      }
+      spec = {
+        dnsNames   = local.ingress_hostnames
+        secretName = local.ingress_cert_secret_name
+        issuerRef = {
+          kind = "ClusterIssuer"
+          name = local.ingress_cert_cluster_issuer_name
+        }
+      }
+    })
+  )
+}
+
+################################
+# Elasticsearch Gateway & Routes
+################################
+
+resource "kubernetes_manifest" "elasticsearch_external_gateway" {
+  count = local.ingress_is_enabled && local.ingress_dns_domain != "" ? 1 : 0
+
+  manifest = yamldecode(
+    yamlencode({
+      apiVersion = "gateway.networking.k8s.io/v1beta1"
+      kind       = "Gateway"
+      metadata = {
+        name      = "${local.resource_id}-external"
+        namespace = local.istio_ingress_namespace
+        labels    = local.final_labels
+      }
+      spec = {
+        gatewayClassName = local.gateway_ingress_class_name
+        addresses = [
+          {
+            type  = "Hostname"
+            value = local.gateway_external_lb_hostname
+          }
+        ]
+        listeners = [
+          {
+            name     = "https-external"
+            hostname = local.elasticsearch_ingress_external_hostname
+            port     = 443
+            protocol = "HTTPS"
+            tls = {
+              mode = "Terminate"
+              certificateRefs = [
+                {
+                  name = local.ingress_cert_secret_name
+                }
+              ]
+            }
+            allowedRoutes = {
+              namespaces = {
+                from = "All"
+              }
+            }
+          },
+          {
+            name     = "http-external"
+            hostname = local.elasticsearch_ingress_external_hostname
+            port     = 80
+            protocol = "HTTP"
+            allowedRoutes = {
+              namespaces = {
+                from = "All"
+              }
+            }
+          }
+        ]
+      }
+    })
+  )
+
+  depends_on = [
+    kubernetes_manifest.ingress_certificate
+  ]
+}
+
+# HTTPRoute to redirect external HTTP->HTTPS
+resource "kubernetes_manifest" "elasticsearch_http_external_redirect" {
+  count = local.ingress_is_enabled && local.ingress_dns_domain != "" ? 1 : 0
+
+  manifest = yamldecode(
+    yamlencode({
+      apiVersion = "gateway.networking.k8s.io/v1beta1"
+      kind       = "HTTPRoute"
+      metadata = {
+        name      = "http-external-redirect"
+        namespace = kubernetes_namespace.elasticsearch_namespace.metadata[0].name
+        labels    = local.final_labels
+      }
+      spec = {
+        hostnames = [
+          local.elasticsearch_ingress_external_hostname
+        ]
+        parentRefs = [
+          {
+            name        = "${local.resource_id}-external"
+            namespace   = local.istio_ingress_namespace
+            sectionName = "http-external"
+          }
+        ]
+        rules = [
+          {
+            filters = [
+              {
+                type = "RequestRedirect"
+                requestRedirect = {
+                  scheme     = "https"
+                  statusCode = 301
+                }
+              }
+            ]
+          }
+        ]
+      }
+    })
+  )
+
+  depends_on = [
+    kubernetes_manifest.elasticsearch_external_gateway
+  ]
+}
+
+# HTTPRoute to route external HTTPS traffic to Elasticsearch
+resource "kubernetes_manifest" "elasticsearch_https_external" {
+  count = local.ingress_is_enabled && local.ingress_dns_domain != "" ? 1 : 0
+
+  manifest = yamldecode(
+    yamlencode({
+      apiVersion = "gateway.networking.k8s.io/v1beta1"
+      kind       = "HTTPRoute"
+      metadata = {
+        name      = "https-external"
+        namespace = kubernetes_namespace.elasticsearch_namespace.metadata[0].name
+        labels    = local.final_labels
+      }
+      spec = {
+        hostnames = [
+          local.elasticsearch_ingress_external_hostname
+        ]
+        parentRefs = [
+          {
+            name        = "${local.resource_id}-external"
+            namespace   = local.istio_ingress_namespace
+            sectionName = "https-external"
+          }
+        ]
+        rules = [
+          {
+            matches = [
+              {
+                path = {
+                  type  = "PathPrefix"
+                  value = "/"
+                }
+              }
+            ]
+            backendRefs = [
+              {
+                name      = local.elasticsearch_kube_service_name
+                namespace = kubernetes_namespace.elasticsearch_namespace.metadata[0].name
+                port      = local.elasticsearch_port
+              }
+            ]
+          }
+        ]
+      }
+    })
+  )
+
+  depends_on = [
+    kubernetes_manifest.elasticsearch_external_gateway
+  ]
+}
+
+################################
+# Kibana Gateway & Routes
+################################
+
+resource "kubernetes_manifest" "kibana_external_gateway" {
+  count = local.ingress_is_enabled && local.ingress_dns_domain != "" ? 1 : 0
+
+  manifest = yamldecode(
+    yamlencode({
+      apiVersion = "gateway.networking.k8s.io/v1beta1"
+      kind       = "Gateway"
+      metadata = {
+        name      = "${local.resource_id}-kb-external"
+        namespace = local.istio_ingress_namespace
+        labels    = local.final_labels
+      }
+      spec = {
+        gatewayClassName = local.gateway_ingress_class_name
+        addresses = [
+          {
+            type  = "Hostname"
+            value = local.gateway_external_lb_hostname
+          }
+        ]
+        listeners = [
+          {
+            name     = "https-external"
+            hostname = local.kibana_ingress_external_hostname
+            port     = 443
+            protocol = "HTTPS"
+            tls = {
+              mode = "Terminate"
+              certificateRefs = [
+                {
+                  name = local.ingress_cert_secret_name
+                }
+              ]
+            }
+            allowedRoutes = {
+              namespaces = {
+                from = "All"
+              }
+            }
+          },
+          {
+            name     = "http-external"
+            hostname = local.kibana_ingress_external_hostname
+            port     = 80
+            protocol = "HTTP"
+            allowedRoutes = {
+              namespaces = {
+                from = "All"
+              }
+            }
+          }
+        ]
+      }
+    })
+  )
+
+  depends_on = [
+    kubernetes_manifest.ingress_certificate
+  ]
+}
+
+# HTTPRoute to redirect external HTTP->HTTPS (Kibana)
+resource "kubernetes_manifest" "kibana_http_external_redirect" {
+  count = local.ingress_is_enabled && local.ingress_dns_domain != "" ? 1 : 0
+
+  manifest = yamldecode(
+    yamlencode({
+      apiVersion = "gateway.networking.k8s.io/v1beta1"
+      kind       = "HTTPRoute"
+      metadata = {
+        name      = "http-kb-external-redirect"
+        namespace = kubernetes_namespace.elasticsearch_namespace.metadata[0].name
+        labels    = local.final_labels
+      }
+      spec = {
+        hostnames = [
+          local.kibana_ingress_external_hostname
+        ]
+        parentRefs = [
+          {
+            name        = "${local.resource_id}-kb-external"
+            namespace   = local.istio_ingress_namespace
+            sectionName = "http-external"
+          }
+        ]
+        rules = [
+          {
+            filters = [
+              {
+                type = "RequestRedirect"
+                requestRedirect = {
+                  scheme     = "https"
+                  statusCode = 301
+                }
+              }
+            ]
+          }
+        ]
+      }
+    })
+  )
+
+  depends_on = [
+    kubernetes_manifest.kibana_external_gateway
+  ]
+}
+
+# HTTPRoute to route external HTTPS traffic to Kibana
+resource "kubernetes_manifest" "kibana_https_external" {
+  count = local.ingress_is_enabled && local.ingress_dns_domain != "" ? 1 : 0
+
+  manifest = yamldecode(
+    yamlencode({
+      apiVersion = "gateway.networking.k8s.io/v1beta1"
+      kind       = "HTTPRoute"
+      metadata = {
+        name      = "https-kb-external"
+        namespace = kubernetes_namespace.elasticsearch_namespace.metadata[0].name
+        labels    = local.final_labels
+      }
+      spec = {
+        hostnames = [
+          local.kibana_ingress_external_hostname
+        ]
+        parentRefs = [
+          {
+            name        = "${local.resource_id}-kb-external"
+            namespace   = local.istio_ingress_namespace
+            sectionName = "https-external"
+          }
+        ]
+        rules = [
+          {
+            matches = [
+              {
+                path = {
+                  type  = "PathPrefix"
+                  value = "/"
+                }
+              }
+            ]
+            backendRefs = [
+              {
+                name      = local.kibana_kube_service_name
+                namespace = kubernetes_namespace.elasticsearch_namespace.metadata[0].name
+                port      = local.kibana_port
+              }
+            ]
+          }
+        ]
+      }
+    })
+  )
+
+  depends_on = [
+    kubernetes_manifest.kibana_external_gateway
+  ]
+}
