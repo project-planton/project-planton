985b44692e429710c38bd71bbe67fb0c4ec410a6
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-02-05 05:55:27 +0530

    implemented terraform module for aws-secrets-manager

--- apis/project/planton/provider/aws/awssecretsmanager/v1/iac/tf/locals.tf
+++ apis/project/planton/provider/aws/awssecretsmanager/v1/iac/tf/locals.tf
@@ -1,9 +1,36 @@
 locals {
-  aws_tags = {
-    Resource      = "true"
-    Organization  = var.metadata.org
-    Environment   = var.metadata.env.id
-    ResourceKind  = "aws-secrets-manager"
-    ResourceId    = var.metadata.id
+  # Derive a stable resource ID
+  resource_id = (
+    var.metadata.id != null && var.metadata.id != ""
+    ? var.metadata.id
+    : var.metadata.name
+  )
+
+  # Base tags
+  base_tags = {
+    "resource"      = "true"
+    "resource_id"   = local.resource_id
+    "resource_kind" = "aws_secrets_manager"
   }
+
+  # Organization tag only if var.metadata.org is non-empty
+  org_tag = (
+  var.metadata.org != null && var.metadata.org != ""
+  ) ? {
+    "organization" = var.metadata.org
+  } : {}
+
+  # Environment tag only if var.metadata.env.id is non-empty
+  env_tag = (
+  var.metadata.env != null &&
+  try(var.metadata.env.id, "") != ""
+  ) ? {
+    "environment" = var.metadata.env.id
+  } : {}
+
+  # Merge base, org, and environment tags
+  final_tags = merge(local.base_tags, local.org_tag, local.env_tag)
+
+  # List of secrets to manage
+  secret_names = try(var.spec.secret_names, [])
 }
--- apis/project/planton/provider/aws/awssecretsmanager/v1/iac/tf/main.tf
+++ apis/project/planton/provider/aws/awssecretsmanager/v1/iac/tf/main.tf
@@ -1,18 +1,22 @@
 resource "aws_secretsmanager_secret" "secrets" {
-  for_each = toset(var.spec.secret_names)
+  for_each = {
+    for name in local.secret_names :
+    name => name
+  }
 
-  name = "${var.metadata.id}-${each.value}"
-
-  tags = local.aws_tags
+  name = format("%s-%s", local.resource_id, each.value)
+  tags = local.final_tags
 }
 
-resource "aws_secretsmanager_secret_version" "placeholder_versions" {
-  for_each = aws_secretsmanager_secret.secrets
+resource "aws_secretsmanager_secret_version" "secrets" {
+  for_each = {
+    for name in local.secret_names :
+    name => name
+  }
 
-  secret_id     = each.value.id
+  secret_id     = aws_secretsmanager_secret.secrets[each.key].id
   secret_string = "placeholder"
 
-  # Ignore changes to secret_string to mimic Pulumi's IgnoreChanges([]string{"secretString"})
   lifecycle {
     ignore_changes = [secret_string]
   }
