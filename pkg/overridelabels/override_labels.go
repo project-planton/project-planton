401b49560f837ece9a4497f9f8f5fbc367e45be0
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-02-26 12:16:43 +0530

    added override labels

---
+++ pkg/overridelabels/override_labels.go
@@ -0,0 +1,7 @@
+package overridelabels
+
+const (
+	KubernetesNamespaceLabelKey    = "kubernetes.project-planton.org/namespace"
+	PulumiStackFqdnLabelKey        = "pulumi.project-planton.org/stack-fqdn"
+	TerraformBackendObjectLabelKey = "terraform.project-planton.org/backend-object"
+)
