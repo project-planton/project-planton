85ac3adf9d80bc3ce01994026da5a8101432f9e2
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-04-10 09:49:52 +0530

    improved tests for validating domain names in aws-cert-manager-cert

--- apis/project/planton/provider/aws/awscertmanagercert/v1/api_test.go
+++ apis/project/planton/provider/aws/awscertmanagercert/v1/api_test.go
@@ -42,4 +42,53 @@ var _ = Describe("AwsCertManagerCert", func() {
 			Expect(err).To(BeNil())
 		})
 	})
+
+	Context("Domain Pattern Validations", func() {
+
+		Context("PrimaryDomainName", func() {
+			It("should accept a valid apex domain", func() {
+				input.Spec.PrimaryDomainName = "example.com"
+				err := protovalidate.Validate(input)
+				Expect(err).To(BeNil())
+			})
+
+			It("should accept a valid wildcard domain", func() {
+				input.Spec.PrimaryDomainName = "*.example.com"
+				err := protovalidate.Validate(input)
+				Expect(err).To(BeNil())
+			})
+
+			It("should reject a domain missing a TLD", func() {
+				input.Spec.PrimaryDomainName = "example"
+				err := protovalidate.Validate(input)
+				Expect(err).NotTo(BeNil())
+			})
+
+			It("should reject multiple wildcard asterisks", func() {
+				input.Spec.PrimaryDomainName = "**.example.com"
+				err := protovalidate.Validate(input)
+				Expect(err).NotTo(BeNil())
+			})
+
+			It("should reject a domain with invalid characters", func() {
+				input.Spec.PrimaryDomainName = "exa@mple.com"
+				err := protovalidate.Validate(input)
+				Expect(err).NotTo(BeNil())
+			})
+		})
+
+		Context("AlternateDomainNames", func() {
+			It("should accept multiple valid domains", func() {
+				input.Spec.AlternateDomainNames = []string{"www.example.com", "*.foo.org"}
+				err := protovalidate.Validate(input)
+				Expect(err).To(BeNil())
+			})
+
+			It("should reject if any domain is invalid", func() {
+				input.Spec.AlternateDomainNames = []string{"www.example.com", "invalid@@domain"}
+				err := protovalidate.Validate(input)
+				Expect(err).NotTo(BeNil())
+			})
+		})
+	})
 })
