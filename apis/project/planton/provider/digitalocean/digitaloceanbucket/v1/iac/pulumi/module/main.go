d9a8b265a2574f816f687b9451b002a7542d29f1
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-07-15 05:12:02 +0530

    fixed build errors

--- apis/project/planton/provider/digitalocean/digitaloceanbucket/v1/iac/pulumi/module/main.go
+++ apis/project/planton/provider/digitalocean/digitaloceanbucket/v1/iac/pulumi/module/main.go
@@ -1,25 +1,10 @@
 package module
 
 import (
-	"github.com/pkg/errors"
 	digitaloceanbucketv1 "github.com/project-planton/project-planton/apis/project/planton/provider/digitalocean/digitaloceanbucket/v1"
-	"github.com/pulumi/pulumi-digitalocean/sdk/v5/go/digitalocean"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func Resources(ctx *pulumi.Context, stackInput *digitaloceanbucketv1.DigitalOceanBucketStackInput) error {
-	digitaloceanCredential := stackInput.ProviderCredential
-	//create digitalocean provider using the credentials from the input
-	_, err := digitalocean.NewProvider(ctx,
-		"digitalocean",
-		&digitalocean.ProviderArgs{
-			ClientId:       pulumi.String(digitaloceanCredential.ClientId),
-			ClientSecret:   pulumi.String(digitaloceanCredential.ClientSecret),
-			SubscriptionId: pulumi.String(digitaloceanCredential.SubscriptionId),
-			TenantId:       pulumi.String(digitaloceanCredential.TenantId),
-		})
-	if err != nil {
-		return errors.Wrap(err, "failed to create digitalocean provider")
-	}
 	return nil
 }
