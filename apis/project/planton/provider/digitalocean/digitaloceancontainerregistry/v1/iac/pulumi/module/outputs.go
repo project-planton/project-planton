21ab7b297e1af89c3e81e6c2cbc72c907c329e1d
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-07-16 19:42:03 +0530

    implemented container-registry digital ocean pulumi module

--- apis/project/planton/provider/digitalocean/digitaloceancontainerregistry/v1/iac/pulumi/module/outputs.go
+++ apis/project/planton/provider/digitalocean/digitaloceancontainerregistry/v1/iac/pulumi/module/outputs.go
@@ -1,5 +1,12 @@
 package module
 
 const (
-	OpOutputKey = "value"
+	// OpRegistryName is the exported stack output name with the registry's name.
+	OpRegistryName = "registry_name"
+
+	// OpServerUrl is the exported stack output name with the registry server URL.
+	OpServerUrl = "server_url"
+
+	// OpRegion is the exported stack output name with the region slug.
+	OpRegion = "region"
 )
