0996704c66fb07f5925ddc8e8cdfaeb569aea590
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-07-23 07:41:46 +0530

    added digital-ocean certificate protos

---
+++ apis/project/planton/provider/digitalocean/digitaloceancertificate/v1/iac/pulumi/module/outputs.go
@@ -0,0 +1,9 @@
+package module
+
+const (
+	// OpBucketId is the exported stack output containing the bucket ID.
+	OpBucketId = "bucket_id"
+
+	// OpEndpoint is the exported stack output containing the regional endpoint.
+	OpEndpoint = "endpoint"
+)
