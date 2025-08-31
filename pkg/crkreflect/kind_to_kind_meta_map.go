8fa89da64dd1ddad5a5ffe2c34905f1c59a9326c
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-08-31 08:19:54 +0530

    added kind to kind meta map

---
+++ pkg/crkreflect/kind_to_kind_meta_map.go
@@ -0,0 +1,27 @@
+package crkreflect
+
+import (
+	"github.com/project-planton/project-planton/apis/project/planton/shared/cloudresourcekind"
+	"google.golang.org/protobuf/proto"
+)
+
+// KindToKindMetaMap builds a map of CloudResourceKind -> CloudResourceKindMeta
+func KindToKindMetaMap() map[cloudresourcekind.CloudResourceKind]*cloudresourcekind.CloudResourceKindMeta {
+	result := make(map[cloudresourcekind.CloudResourceKind]*cloudresourcekind.CloudResourceKindMeta)
+
+	for _, kind := range KindsList() {
+		val := kind.Descriptor().Values().ByNumber(kind.Number())
+		if val == nil {
+			continue
+		}
+		ext := proto.GetExtension(val.Options(), cloudresourcekind.E_KindMeta)
+		if meta, ok := ext.(*cloudresourcekind.CloudResourceKindMeta); ok {
+			if meta == nil {
+				continue
+			}
+			result[kind] = meta
+		}
+	}
+
+	return result
+}
