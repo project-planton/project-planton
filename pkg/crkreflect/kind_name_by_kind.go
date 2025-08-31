3cbcafe933f8216df3d2046af8fdff2f97329702
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-08-31 07:37:38 +0530

    added kind-name by kind util

---
+++ pkg/crkreflect/kind_name_by_kind.go
@@ -0,0 +1,15 @@
+package crkreflect
+
+import (
+	"github.com/project-planton/project-planton/apis/project/planton/shared/cloudresourcekind"
+	log "github.com/sirupsen/logrus"
+)
+
+func ExtractKindNameByKind(kind cloudresourcekind.CloudResourceKind) string {
+	kindMeta, err := KindMeta(kind)
+	if err != nil {
+		log.Errorf("failed to extract kind meta by kind %s", kind.String())
+		return ""
+	}
+	return kindMeta.Name
+}
