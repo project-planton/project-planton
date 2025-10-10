ec2876734ffd02d7d0b68a7926700dd87a23efd7
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-10-10 18:36:35 +0530

    added changelog for percona-server-mongodb-operator

--- apis/project/planton/provider/kubernetes/addon/perconaservermongodboperator/v1/iac/pulumi/module/vars.go
+++ apis/project/planton/provider/kubernetes/addon/perconaservermongodboperator/v1/iac/pulumi/module/vars.go
@@ -9,5 +9,5 @@ var vars = struct {
 	DefaultNamespace: "percona-operator",
 	HelmChartName:    "psmdb-operator",
 	HelmChartRepo:    "https://percona.github.io/percona-helm-charts/",
-	HelmChartVersion: "1.16.0",
+	HelmChartVersion: "1.20.1",
 }
