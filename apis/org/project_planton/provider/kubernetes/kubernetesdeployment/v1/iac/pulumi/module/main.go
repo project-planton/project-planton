e0dc776ae2dfc0a40010aecd64999308fe4aa5ca
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-11-22 21:11:01 +0530

    fixed nil pointer when checking for ingress in kubernetes deployment pulumi module

--- apis/org/project_planton/provider/kubernetes/kubernetesdeployment/v1/iac/pulumi/module/main.go
+++ apis/org/project_planton/provider/kubernetes/kubernetesdeployment/v1/iac/pulumi/module/main.go
@@ -53,7 +53,7 @@ func Resources(ctx *pulumi.Context, stackInput *kubernetesdeploymentv1.Kubernete
 	}
 
 	//create istio-ingress resources if ingress is enabled.
-	if locals.KubernetesDeployment.Spec.Ingress.Enabled {
+	if locals.KubernetesDeployment.Spec.Ingress != nil && locals.KubernetesDeployment.Spec.Ingress.Enabled {
 		if err := ingress(ctx, locals, kubernetesProvider, createdNamespace); err != nil {
 			return errors.Wrap(err, "failed to create istio ingress resources")
 		}
